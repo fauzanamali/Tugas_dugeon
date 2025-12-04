f29110a429d2446fbb9b6ec6645d7d81dd46c223
fauzanamali <fauzan0312@student.abudzar.sch.id>
2025-12-04 09:13:28 +0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 export default {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
